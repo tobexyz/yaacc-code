895c0ce703a247b2c6772eb6dcc6cdb91aab42bb
tobexyz <tobexyz@schoenesnetz.de>
2018-12-15 21:34:05 +0100

    wip ci

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo $(pwd)
-docker run -v $(pwd):/home/yaacc/yaacc-code tobexyz/yaacc-ci:latest bash -c 'cd /home/yaacc/yaacc-code && ls -la && ./gradlew build'
+chmod a+w $(pwd)
+docker run -v $(pwd):/home/yaacc/yaacc-code tobexyz/yaacc-ci:0.0.1 bash -c 'cd /home/yaacc/yaacc-code && ls -la && ./gradlew build'
 
