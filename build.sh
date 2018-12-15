fb619c77c2c7a8fc775140629826541a34cfa75e
tobexyz <tobexyz@schoenesnetz.de>
2018-12-15 20:51:21 +0100

    wip ci

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 echo $(pwd)
 echo '*****************'
 ls 
-docker run -v $(pwd):/home/yaacc/yaacc-code tobexyz/yaacc-ci:0.0.1 bash -c 'cd /home/yaacc/yaacc-code/ && ./gradlew build'
+docker run -v $(pwd):/home/yaacc/ tobexyz/yaacc-ci:0.0.1 bash -c 'cd /home/yaacc/ && ls  && ./gradlew build'
 
